3c58f53191edcfc66599c3a95c63bddf57975266
Nadya Djojosantoso <hello@nadyafebi.com>
2020-08-15 11:55:16 -0700

    Fix routing

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'build/index.html'));
+});
 app.listen(PORT, () => {
    console.log(`Server running on port ${PORT}.`); 
 });
